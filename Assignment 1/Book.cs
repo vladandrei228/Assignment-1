f37ef3d096b8860d82cfbb58453c743372f4c29b
Vlad Andrei Gruia <vlad0765@edu.easj.dk>
2020-10-15 13:57:30 +0300

    Book class

---
+++ Assignment 1/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1
+{
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private int _pageNumber;
+        private string _isbn13;
+
+        public Book(string Title, string Author, int pageNumber, string isbn13)
+        {
+            _title = Title;
+            _author = Author;
+            _pageNumber = pageNumber;
+            _isbn13 = isbn13;
+        }
+    }
+}
