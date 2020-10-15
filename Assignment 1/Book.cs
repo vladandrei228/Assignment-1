9f947a6c163e8ac32c01097049353d9369ee071c
Vlad Andrei Gruia <vlad0765@edu.easj.dk>
2020-10-15 14:07:12 +0300

    Book class done!

--- Assignment 1/Book.cs
+++ Assignment 1/Book.cs
@@ -11,12 +11,59 @@ namespace Assignment_1
         private int _pageNumber;
         private string _isbn13;
 
-        public Book(string Title, string Author, int pageNumber, string isbn13)
+        public Book(string Title, string Author, int pageNumber, string Isbn13)
         {
-            _title = Title;
-            _author = Author;
-            _pageNumber = pageNumber;
-            _isbn13 = isbn13;
+            title = Title;
+            author = Author;
+            pgnr = pageNumber;
+            isbn13 = Isbn13;
         }
+
+        public string title
+        {
+            get { return _title;}
+            set
+            {
+                if (value.Length < 2) 
+                {
+                    throw new ArgumentException("Title must contain at minimum 2 chacaters");
+                }
+
+                _title = value;
+            }
+        }
+
+        public string author { get; set; }
+
+        public int pgnr
+        {
+            get
+            {
+                return _pageNumber;
+            }
+            set
+            {
+                if (value <= 10 || value > 1000)
+                {
+                    throw new ArgumentException(" Page number has to be between 11 and 1000");
+                }
+
+                _pageNumber = value;
+            }
+        }
+
+        public string isbn13
+        {
+            get { return _isbn13; }
+            set {
+                if (value.Length != 13)
+                {
+                    throw new ArgumentException("ISBN has to be exactly 13 characters");
+                }
+
+                _isbn13 = value;
+            }
+        }
+
     }
 }
